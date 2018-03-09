ec2669c9bfe3cab7e9564914cd047cecaff55da5
nicholas hays <nicholas.hays@hotmail.com>
2018-03-08 22:40:20 -0800

    added comments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+/*
+Author: Nicholas Hays
+Date: 3/8/2018
+Problem: Hackerrank - Algorithms - Bit Manipulation - Cipher
+*/
+
 #include<iostream>
 #include<string>
 
-
 /*
 Given an encoded binary string, enc, its decoded length, n, and k, the number
-of times enc is XOR'd with itself shifted by 0,1...K-1 bits at each operation, 
+of times enc is XOR'd with a copy of enc shifted by 0, 1...K-1 bits, 
 determines the original, decoded, binary string.
 
 i.e
@@ -15,11 +20,15 @@ sample input:
 sample output:
 1001010
 
-reasoning:
-We have a decoded binary string.
-We the first bit of th
+Reasoning:
+The first bit is always the same for both the encoded and decoded string
+because any bit XOR'd with 0 is always itself. b ^ 0 = b.
 
+To solve for the decoded bit, d, at postion x we must know 2 things.
+p = output of the XOR sum of the decoded bits starting at position x - k, if x >= k, otherwise 0, to x - 1. i.e XORSum(x - k || 0, x - 1)
+e = encoded bit at position x.
 
+Once we determine p and e, solving for d is simple, d = p ^ e.
 
 1001010
  1001010
@@ -55,7 +64,7 @@ program sample input call
 
 main < ./input.txt
 
-where sample input.txt:
+where sample input.txt is of the form:
 n k
 s + k -1
 
